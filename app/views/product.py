566672f162dce8d63083d41263bef718ad5129e9
dorothykiz1 <kizdorothy@gmail.com>
2018-10-18 00:52:38 +0300

    Modifies file for the POST endpoint [finishes #161252536]

--- app/views/product.py
+++ app/views/product.py
@@ -10,35 +10,36 @@ def create_product():
 
     data = request.get_json()
     product = dict(
-        Id=len(products) + 1,
-        category=data['category'],
+        # Id=len(products) + 1,
+        title=data['title'],
         description=data['description'],
-        quantity=data['quantity']
+        quantity=data['quantity'],
+        price=data['price']
     )
     products.append(product)
     return json.dumps({'message': 'Product successfully added'}), 201
 
 
-@app.route('/api/v1/products', methods=['GET'])
-def get_all_products():
-    """endpoint to get all products"""
-    return json.dumps({"Products": products}), 200
+# @app.route('/api/v1/products', methods=['GET'])
+# def get_all_products():
+#     """endpoint to get all products"""
+#     return json.dumps({"Products": products}), 200
 
 
-@app.route('/api/v1/products/<int:productId>', methods=['GET'])
-def get_single_product(productId):
-    """ endpoint to get a single product"""
+# @app.route('/api/v1/products/<int:productId>', methods=['GET'])
+# def get_single_product(productId):
+#     """ endpoint to get a single product"""
 
-    for product in products:
-        if product.productId == productId:
-            product_dict = {
-                'category': product.category,
-                'description': product.description,
-                'quantity': product.quantity
-            }
-            products.append(product_dict)
-    # return json.dumps({'Message': "Your request is successful"}), 200
-        if product not in products:
-            return {'message': 'product not in inventory'}, 200
-        else:
-            return json.dumps({'product': products[productId]}), 200
+#     for product in products:
+#         if product.productId == productId:
+#             product_dict = {
+#                 'category': product.category,
+#                 'description': product.description,
+#                 'quantity': product.quantity
+#             }
+#             products.append(product_dict)
+#     # return json.dumps({'Message': "Your request is successful"}), 200
+#         if product not in products:
+#             return {'message': 'product not in inventory'}, 200
+#         else:
+#             return json.dumps({'product': products[productId]}), 200
