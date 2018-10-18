c79c5411e448fbd35cea499d62f883cc125c3fd3
dorothykiz1 <kizdorothy@gmail.com>
2018-10-18 20:25:25 +0300

    Add an endpoint for adding asale record [finishes #161253710]

--- app/views/sale.py
+++ app/views/sale.py
@@ -1,43 +1,33 @@
-from flask import Flask, json, request
-
-app = Flask(__name__)
+from flask import Blueprint, request, json
+mod = Blueprint('sales', __name__)
 sales = []
 
 
-@app.route('/api/v1/sales', methods=['POST'])
+@mod.route('/', methods=['POST'])
 def create_new_sale():
-    """endpoint to post a sale_record"""
+    """endpoint to post a new sale_record"""
 
     data = request.get_json()
     sale = dict(
-        saleId=len(sales)+1,
-        attendant_Id=data['attendant_Id']
+        id=len(sales)+1,
+        attendant_Id=data['attendant_Id'],
+        attendant_name=data['attendant_name'],
     )
     sales.append(sale)
-    return json.dumps({'message': 'sale record successfully made'}), 201
+    return json.dumps({'message': sale}), 201
 
 
-# @app.route('/api/v1/sales', methods=['GET'])
+# @mod.route('/', methods=['GET'])
 # def get_all_sales():
 #     """endpoint to get all sale records"""
 #     return json.dumps({"sales": sales}), 200
 
 
-# @app.route('/api/v1/sales/<int:saleId>', methods=['GET'])
+# @mod.route('/<int:saleId>', methods=['GET'])
 # def get_single_sale_record(saleId):
 #     """ endpoint to get a single sale"""
-
 #     for sale in sales:
-#         if sale.saleId == saleId:
-#             sale_dict = {
-#                 'sale_record_Id': sale.sale_record_Id,
-#                 'attendant_Id': sale.attendant_Id
-#             }
-#             sales.append(sale_dict)
-#     # return json.dumps({'Message': "Your request is successful"}), 200
-#         if sale not in sales:
-#             return {'message': 'sale not made'}, 200
-#         else:
-#             return json.dumps({'sale made': sale_dict})
+#         if sale['id'] == saleId:
+#             return json.dumps({'Message': sale}), 200
 
-#             # return json.dumps({'sale': sales[saleId]}), 200
+#     return json.dumps({'Message': 'Sale Id out of range'}), 400
