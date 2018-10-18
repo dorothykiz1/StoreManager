3a35a5fb2ce020095f872dac55d4ecead1c9d913
dorothykiz1 <kizdorothy@gmail.com>
2018-10-19 00:06:58 +0300

    Modify get add product file by adding Blueprints[finishes #161252536]

---
+++ app/views/__init__.py
@@ -0,0 +1,7 @@
+from flask import Flask
+from app.views import product
+from app.views import sale
+
+app = Flask(__name__)
+app.register_blueprint(product.mod, url_prefix='/api/v1/products')
+# app.register_blueprint(sale.mod, url_prefix='/api/v1/sales')
