9ed3622b2a1d19bc62cc9265831810a61eafa917
dorothykiz1 <kizdorothy@gmail.com>
2018-10-19 00:55:05 +0300

    Add api endpooints for sales

---
+++ app/views/sale.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, request, json
+mod = Blueprint('sales', __name__)
+sales = []
+
+
+@mod.route('/', methods=['POST'])
+def create_new_sale():
+    """endpoint to post a new sale_record"""
+
+    data = request.get_json()
+    sale = dict(
+        id=len(sales)+1,
+        attendant_Id=data['attendant_Id'],
+        attendant_name=data['attendant_name'],
+    )
+    sales.append(sale)
+    return json.dumps({'message': sale}), 201
+
+
+@mod.route('/', methods=['GET'])
+def get_all_sales():
+    """endpoint to get all sale records"""
+    return json.dumps({"sales": sales}), 200
+
+
+@mod.route('/<int:saleId>', methods=['GET'])
+def get_single_sale_record(saleId):
+    """ endpoint to get a single sale"""
+    for sale in sales:
+        if sale['id'] == saleId:
+            return json.dumps({'Message': sale}), 200
+
+    return json.dumps({'Message': 'Sale Id out of range'}), 400
