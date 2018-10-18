eb69c5fb208b7172560f03f17bae8ce9ece9a1e4
dorothykiz1 <kizdorothy@gmail.com>
2018-10-19 00:46:06 +0300

    Modify add product file by adding Blueprints[finishes #161252536]

--- app/views/product.py
+++ app/views/product.py
@@ -1,37 +1,36 @@
 from flask import Blueprint, request, json
-mod = Blueprint('sales', __name__)
 
+mod = Blueprint('products', __name__)
 products = []
 
 
-@mod.route('/api/v1/products', methods=['POST'])
+@mod.route('/', methods=['POST'])
 def create_product():
     """endpoint to post a product"""
 
     data = request.get_json()
     product = dict(
-        id=len(products)+1,
-        title=data['title'],
+        Id=len(products) + 1,
+        category=data['category'],
         description=data['description'],
-        quantity=data['quantity'],
-
+        quantity=data['quantity']
     )
     products.append(product)
     return json.dumps({'message': 'Product successfully added'}), 201
 
 
-# @app.route('/api/v1/products', methods=['GET'])
+# @mod.route('/', methods=['GET'])
 # def get_all_products():
 #     """endpoint to get all products"""
 #     return json.dumps({"Products": products}), 200
 
 
-# @app.route('/api/v1/products/<int:productId>', methods=['GET'])
+# @mod.route('/<int:productId>', methods=['GET'])
 # def get_single_product(productId):
 #     """ endpoint to get a single product"""
 
 #     for product in products:
-#         if product['id']== productId:
+#         if product['id'] == productId:
 #             return json.dumps({'Message': product}), 200
 
-#     return json.dumps({'Message': 'product Id out of range'}), 400
+#     return json.dumps({'Message': 'Sale Id out of range'}), 400
