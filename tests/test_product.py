170c2fddd379897fd85cd8a90c185ca2e3ac2648
dorothykiz1 <kizdorothy@gmail.com>
2018-10-19 14:06:19 +0300

    Add tests to endpoints in product.py[finishes #161341110]

---
+++ tests/test_product.py
@@ -0,0 +1,34 @@
+from app.views import product, sale
+from app.views import app
+import json
+import pytest
+
+
+@pytest.fixture()
+def client():
+    test_client = app.test_client()
+    return test_client
+
+
+class TestProduct:
+    """Testing the product class"""
+
+    def test_create_product(self, client):
+        """testing if user can create a product"""
+        resp = client.post('/', content_type="application/json", data=json.dumps({
+            "title": "Off-shoulder dress",
+            "description": "stripped dress in only two shades(blue,red)",
+            "quantity": 4
+        }
+        ))
+        assert resp.status_code == 201
+
+    def test_get_all_products(self, client):
+        """testing if user can get all products"""
+        resp = client.get('/', content_type="application/json")
+        assert resp.status_code == 200
+
+    def test_get_single_product(self, client):
+        """testing if user can get all products"""
+        resp = client.get('/productId', content_type="application/json")
+        assert resp.status_code == 200
