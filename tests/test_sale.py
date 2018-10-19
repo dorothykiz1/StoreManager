32687420154a0a76b95d629d7c4ef4073903b3c6
dorothykiz1 <kizdorothy@gmail.com>
2018-10-19 14:04:08 +0300

    Add tests to endpoints in sale.py[finishes #161341044

---
+++ tests/test_sale.py
@@ -0,0 +1,33 @@
+from app.views import sale
+from app.views import app
+import json
+import pytest
+
+
+@pytest.fixture()
+def client():
+    test_client = app.test_client()
+    return test_client
+
+
+class TestSale:
+    """Testing the sale class"""
+
+    def test_create_new_sale(self, client):
+        """testing if asale attendant can create a sale record"""
+        resp = client.post('/', content_type="application/json", data=json.dumps({
+            "attendant_Id": 234567,
+            "attendant_name": "Kabarozi",
+        }
+        ))
+        assert resp.status_code == 201
+
+    def test_get_all_sales(self, client):
+        """testing if admin can get all sale records"""
+        resp = client.get('/', content_type="application/json")
+        assert resp.status_code == 200
+
+    def test_get_single_sale_record(self, client):
+        """testing if admin/attendant can get asingle sale records"""
+        resp = client.get('/saleId', content_type="application/json")
+        assert resp.status_code == 200
